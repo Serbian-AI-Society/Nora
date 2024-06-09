1a18aa47650f08be128feff09a5e8475a2ca0af0
Jovana Jovanovic <jovana.jovanovic@uns.ac.rs>
2024-06-09 13:08:05 +0200

    Update Title.jsx

--- frontend/src/components/Chatbot/Title.jsx
+++ frontend/src/components/Chatbot/Title.jsx
@@ -17,7 +17,7 @@ const Title = () => {
             d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0  012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
         </svg>
       </div>
-      <div className="ml-2 font-bold text-2xl">DunavNET Chatbot</div>
+      <div className="ml-2 font-bold text-2xl">Nova</div>
     </div>
   );
 };
